9383177e7c11c11ab4e112be7c775b87d7a198fd
Isabela Pinheiro <belaspinheiro@gmail.com>
2021-02-14 01:28:55 -0300

    First commit: folha de ponto application created.

---
+++ src/main/java/com/ilia/digital/folhadepontoapplication/FolhadepontoapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.ilia.digital.folhadepontoapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolhadepontoapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolhadepontoapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilia/digital/folhadepontoapplication/FolhadepontoapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilia.digital.folhadepontoapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolhadepontoapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
